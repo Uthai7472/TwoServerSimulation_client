d986bbb150a86439b29d0e9ccf913fffebbfc8ce
Uthai Khantamongkhon (SDM) <uthai.khantamongkhon.a7p@ap.denso.com>
2024-09-08 19:18:18 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import io from 'socket.io-client';
 
+const socket = io('https://two-server-simulation-server.vercel.app', {
+  transports: ['websocket', 'polling'],
+});
 function App() {
     const [command, setCommand] = useState('');
     const [randomValue, setRandomValue] = useState();
@@ -15,23 +19,34 @@ function App() {
         }
     };
 
+
     useEffect(() => {
-      const fetchRandomValue = async () => {
-          try {
-              const response = await axios.get('https://two-server-simulation-server.vercel.app/random');
-              setRandomValue(response.data); // Update state with the received random value
-              console.log('Received random value:', response.data);
-          } catch (error) {
-              console.error('Error fetching random value:', error);
-          }
-      };
+      socket.on('connect', () => {
+          console.log('Connected to server');
+      });
 
-        // Fetch random value every second
-        const intervalRandomValue = setInterval(fetchRandomValue, 1000);
+      socket.on('randomValue', (value) => {
+          setRandomValue(value);
+          console.log('Received random value:', value);
+      });
+      
 
-        // Cleanup interval on component unmount
-        return () => clearInterval(intervalRandomValue);
-    }, [randomValue]); 
+      // const fetchRandomValue = async () => {
+      //     try {
+      //         const response = await axios.get('https://two-server-simulation-server.vercel.app/random');
+      //         setRandomValue(response.data); // Update state with the received random value
+      //         console.log('Received random value:', response.data);
+      //     } catch (error) {
+      //         console.error('Error fetching random value:', error);
+      //     }
+      // };
+
+      //   // Fetch random value every second
+      //   const intervalRandomValue = setInterval(fetchRandomValue, 1000);
+
+      //   // Cleanup interval on component unmount
+      //   return () => clearInterval(intervalRandomValue);
+    }, []); 
 
     return (
         <div className="App">
