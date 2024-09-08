3655b137493bd68ccbf106ad18b98ce0ca6edb4f
Uthai Khantamongkhon (SDM) <uthai.khantamongkhon.a7p@ap.denso.com>
2024-09-08 16:32:23 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+    const [command, setCommand] = useState('');
+    const [randomValue, setRandomValue] = useState();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post('http://localhost:5001/command', { command });
+            alert('Command sent to server1');
+        } catch (error) {
+            console.error('Error sending command:', error);
+        }
+    };
+
+    useEffect(() => {
+      const fetchRandomValue = async () => {
+          try {
+              const response = await axios.get('http://localhost:5001/random');
+              setRandomValue(response.data); // Update state with the received random value
+              console.log('Received random value:', response.data);
+          } catch (error) {
+              console.error('Error fetching random value:', error);
+          }
+      };
+
+        // Fetch random value every second
+        const intervalRandomValue = setInterval(fetchRandomValue, 1000);
+
+        // Cleanup interval on component unmount
+        return () => clearInterval(intervalRandomValue);
+    }, [randomValue]); 
+
+    return (
+        <div className="App">
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={command}
+                    onChange={(e) => setCommand(e.target.value)}
+                    placeholder="Enter command"
+                />
+                <button type="submit">Send Command</button>
+            </form>
+            <div>
+                <h2>Random Value: {randomValue}</h2>
+            </div>
+        </div>
+    );
+}
+
+export default App;
