9577064e68c6b658f84ab93408b5544640ee7f72
Uthai Khantamongkhon (SDM) <uthai.khantamongkhon.a7p@ap.denso.com>
2024-09-19 10:39:49 +0700

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3000');
+const socket = io('https://two-server-simulation-server.vercel.app');
 
 const DataDisplay = () => {
     const [data, setData] = useState(null);
