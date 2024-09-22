431cc8e1747d31b1f21c18fcf29705ef373f0996
Uthai Khantamongkhon (SDM) <uthai.khantamongkhon.a7p@ap.denso.com>
2024-09-22 08:36:50 +0700

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3000'); // Replace with your public server's URL
+const socket = io('https://two-server-simulation-server.vercel.app'); // Replace with your public server's URL
 
 const App = () => {
     const [messages, setMessages] = useState([]);
